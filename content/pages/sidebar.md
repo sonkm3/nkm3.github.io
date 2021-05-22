bca98bfb55cddcbf5de763d02a6b3b27c996e263
sonkm3 <sonkm3@gmail.com>
2021-05-22 17:51:42 +0900

    update theme.

---
+++ content/pages/sidebar.md
@@ -0,0 +1,14 @@
+Title: sidebar
+Date: 2021-05-22 00:00
+Modified: 2021-05-22 00:00
+Category: Python
+Tags: pelican
+Slug: sidebar
+Authors: sonkmr
+Summary: Short version for sidebar
+
+sidebar
+
+sidebar
+
+sidebar
--- pelicanconf.py
+++ pelicanconf.py
@@ -18,6 +18,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+DEFAULT_DATE_FORMAT = ('%Y-%m-%d')
+
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
@@ -31,4 +33,13 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+THEME = 'themes/blue-penguin'
+
+MENUITEMS = (
+    ('About', 'pages/about.html'),
+    ('Archives', 'archives.html'),
+)
+
+DISPLAY_PAGES_ON_MENU = False
